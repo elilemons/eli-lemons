a4fbd24d1f6f5de8c73f56ca3aafb627e62e4c60
Eli Lemons <me@elilemons.com>
2025-04-25 15:06:55 -0400

    Seperated dot out for easier management

--- src/blocks/LinkBlock/Component.tsx
+++ src/blocks/LinkBlock/Component.tsx
@@ -3,6 +3,7 @@ import { CMSLink } from '../../components/Link'
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
 import { cn } from '@/utilities/ui'
+import { Fragment } from 'react'
 
 type Props = LinkBlockProps & {
   className?: string
@@ -32,21 +33,29 @@ export const LinkBlock: React.FC<Props> = async ({ className, links = [] }) => {
   return (
     <div className="container my-16 flex w-full items-end flex-col gap-3">
       {fetchedLinks.docs.map((link) => (
-        <CMSLink
-          key={link.id}
-          className={cn(
-            'text-white relative lowercase text-6xl pr-5',
-            'after:content-["•"] after:absolute after:top-[10px]',
-            'transition-[letter-spacing] left-[inherit] duration-300 ease-in-out tracking-normal',
-            'hover:tracking-widest',
-            'hover:after:animate-slide-left-right hover:after:text-[#FFBE00]',
-            '!motion-reduce:transition-none hover:after:!motion-reduce:animation-none',
-            className,
-          )}
-          label={link.label}
-          newTab={link.newTab}
-          url={link.url}
-        />
+        <div key={link.id} className="relative text-6xl group hover:cursor-pointer">
+          <CMSLink
+            className={cn(
+              'relative lowercase pr-5 z-20',
+              'transition-[letter-spacing] left-[inherit] duration-300 ease-in-out tracking-normal',
+              'group-hover:tracking-widest',
+              '!motion-reduce:transition-none group-hover:after:!motion-reduce:animation-none',
+              className,
+            )}
+            label={link.label}
+            newTab={link.newTab}
+            url={link.url}
+          />
+          <div
+            className={cn(
+              'absolute top-8 right-0 z-10',
+              'group-hover:animate-slide-left-right group-hover:text-brand',
+              '!motion-reduce:transition-none group-hover:!motion-reduce:animation-none',
+            )}
+          >
+            •
+          </div>
+        </div>
       ))}
     </div>
   )
