c195f6bb0988e157447b170732e8c899c1c7adde
Eli Lemons <me@elilemons.com>
2025-04-25 17:38:10 -0400

    Light is controlled by gyroscope

--- src/components/Light/index.tsx
+++ src/components/Light/index.tsx
@@ -1,10 +1,22 @@
+'use client'
+import { useGyroscope } from '@/providers/Gyroscope'
 import { cn } from '@/utilities/ui'
-import * as React from 'react'
+import { useEffect, useRef } from 'react'
 
 export const Light: React.FC = () => {
+  const { position, requestPermission } = useGyroscope()
+  const lightEffectRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (lightEffectRef.current) {
+      lightEffectRef.current.style.transform = `translate(${position.x}%, ${position.y}%)`
+    }
+  }, [position])
+
   return (
     <div className="w-full flex justify-center items-center absolute min-h-screen z-[1]">
       <div
+        ref={lightEffectRef}
         className={cn(
           'rounded-full',
           'size-[200px] blur-[20px] shadow-[0_0_60px_20px_rgba(255,224,0,0.5)]',
