1f2de45a212afb2875aa0572260b6d6cdeebdd57
Eli Lemons <me@elilemons.com>
2025-04-25 17:37:57 -0400

    Footer uses permission button

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -1,33 +1,23 @@
-import { getCachedGlobal } from '@/utilities/getGlobals'
+'use client'
 import Link from 'next/link'
-import React from 'react'
-
-import type { Footer } from '@/payload-types'
+import { FC } from 'react'
 
 import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
-import { CMSLink } from '@/components/Link'
-import { Logo } from '@/components/Logo/Logo'
 import Lemon from '@/components/Lemon'
+import { GyroscopePermissionButton } from '@/components/GyroscopePermissionButton'
 
-export async function Footer() {
-  const footerData: Footer = await getCachedGlobal('footer', 1)()
-
-  const navItems = footerData?.navItems || []
-
+export const Footer: FC = () => {
   return (
     <footer className="mt-auto">
-      <div className="container py-8 gap-8 flex flex-row justify-between">
+      <div className="container py-8 gap-8 flex flex-row justify-between items-end">
         <Link className="flex items-center" href="/">
           <Lemon />
         </Link>
 
+        <GyroscopePermissionButton />
+
         <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
           <ThemeSelector />
-          <nav className="flex flex-col md:flex-row gap-4">
-            {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-white" key={i} {...link} />
-            })}
-          </nav>
         </div>
       </div>
     </footer>
