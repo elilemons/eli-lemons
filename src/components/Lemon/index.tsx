d5bf6386b78181f56c70061946e7a7a560b45f13
Eli Lemons <me@elilemons.com>
2025-05-01 14:28:55 -0400

    Added class name and viewbox

--- src/components/Lemon/index.tsx
+++ src/components/Lemon/index.tsx
@@ -1,16 +1,19 @@
 import { SVGProps, Ref, FC } from 'react'
 
 type Props = {
+  className?: string
   props?: SVGProps<SVGSVGElement>
   ref?: Ref<SVGSVGElement>
 }
 
-const Lemon: FC<Props> = ({ props, ref }) => {
+const Lemon: FC<Props> = ({ className, props, ref }) => {
   return (
     <svg
+      className={className}
       xmlns="http://www.w3.org/2000/svg"
       width={props?.width ?? '70'}
       height={props?.height ?? '69'}
+      viewBox="0 0 70 69"
       fill="none"
       ref={ref}
       {...props}
