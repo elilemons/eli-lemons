073a6494b9c604cdbcf6bef8c628423281e3df34
Eli Lemons <me@elilemons.com>
2025-05-05 14:47:27 -0400

    Rolodex begins

--- src/blocks/LinkBlock/Component.tsx
+++ src/blocks/LinkBlock/Component.tsx
@@ -5,6 +5,7 @@ import configPromise from '@payload-config'
 import { cn } from '@/utilities/ui'
 import ArrowUp from '@/components/Icons/arrow-up'
 import ArrowDown from '@/components/Icons/arrow-down'
+import { Rolodex } from '@/components/Rolodex'
 
 type Props = LinkBlockProps & {
   className?: string
@@ -31,45 +32,5 @@ export const LinkBlock: React.FC<Props> = async ({ className, links = [] }) => {
       : {}),
   })
 
-  const arrowIconClasses =
-    'size-8 group-hover:animate-bounce-up group-hover:repeat-infinite group-hover:text-brand transition-colors duration-1000 dark:text-white light:text-black'
-
-  return (
-    <div className="container my-16 flex flex-col gap-10">
-      <div className="flex flex-row justify-center items-center w-full group cursor-pointer z-20">
-        <ArrowUp className={arrowIconClasses} />
-      </div>
-      <div className="flex flex-col w-full items-end">
-        {fetchedLinks.docs.map((link) => (
-          <div key={link.id} className="relative text-6xl group hover:cursor-pointer">
-            <CMSLink
-              className={cn(
-                'font-black',
-                'relative lowercase pr-5 z-20',
-                'transition-[letter-spacing] left-[inherit] duration-300 ease-in-out tracking-normal',
-                'group-hover:tracking-widest',
-                '!motion-reduce:transition-none group-hover:after:!motion-reduce:animation-none',
-                className,
-              )}
-              label={link.label}
-              newTab={link.newTab}
-              url={link.url}
-            />
-            <div
-              className={cn(
-                'absolute top-8 right-0 z-10',
-                'group-hover:animate-slide-left-right group-hover:text-brand',
-                '!motion-reduce:transition-none group-hover:!motion-reduce:animation-none',
-              )}
-            >
-              •
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="flex flex-row justify-center items-center w-full group cursor-pointer z-20">
-        <ArrowDown className={arrowIconClasses} />
-      </div>
-    </div>
-  )
+  return <Rolodex className={className} links={fetchedLinks.docs} />
 }
---
+++ src/components/Rolodex/index.tsx
@@ -0,0 +1,92 @@
+'use client'
+import type { Link, LinkBlock as LinkBlockProps } from '@/payload-types'
+import { CMSLink } from '../../components/Link'
+import { cn } from '@/utilities/ui'
+import ArrowUp from '@/components/Icons/arrow-up'
+import ArrowDown from '@/components/Icons/arrow-down'
+import { useState } from 'react'
+
+type Props = {
+  className?: string
+  links: Link[]
+}
+
+export const Rolodex: React.FC<Props> = ({ className, links }) => {
+  const [activeIndex, setActiveIndex] = useState<number>(0)
+
+  const bounce = (dir: 'up' | 'down') => {
+    const el = document.getElementById('link-block')
+    if (el) {
+      el.classList.add(dir === 'up' ? 'bounce-up' : 'bounce-down')
+      setTimeout(() => el.classList.remove('bounce-up', 'bounce-down'), 300)
+    }
+  }
+
+  const moveUp = () => {
+    if (activeIndex > 0) setActiveIndex((i) => i - 1)
+    else bounce('up')
+  }
+
+  const moveDown = () => {
+    if (activeIndex < links.length - 1) setActiveIndex((i) => i + 1)
+    else bounce('down')
+  }
+
+  const arrowIconClasses =
+    'size-8 group-hover:animate-bounce-up group-hover:repeat-infinite group-hover:text-brand transition-colors duration-1000 dark:text-white light:text-black'
+
+  return (
+    <div className="container my-16 flex flex-col gap-10">
+      <button
+        className="flex flex-row justify-center items-center w-full group cursor-pointer z-20"
+        onClick={moveUp}
+      >
+        <ArrowUp className={arrowIconClasses} />
+      </button>
+      <div className="flex flex-col w-full items-end">
+        {links?.map((link, i) => (
+          <div
+            key={link.id}
+            className={cn(
+              'relative group hover:cursor-pointer',
+              'text-6xl',
+              activeIndex === i ? 'scale-100' : 'scale-75',
+              'transition-all',
+            )}
+          >
+            <CMSLink
+              className={cn(
+                'font-black',
+                'relative lowercase pr-5 z-20',
+                'transition-[letter-spacing] left-[inherit] duration-300 ease-in-out tracking-normal',
+                '!motion-reduce:transition-none group-hover:after:!motion-reduce:animation-none',
+                activeIndex === i ? 'scale-100 tracking-widest' : 'scale-75',
+                className,
+              )}
+              label={link.label}
+              newTab={link.newTab}
+              url={link.url}
+            />
+            <div
+              className={cn(
+                'absolute top-8 right-0 z-10',
+                '!motion-reduce:transition-none group-hover:!motion-reduce:animation-none',
+                activeIndex === i
+                  ? 'animate-slide-left-right text-brand '
+                  : 'light:text-black dark:text-white',
+              )}
+            >
+              •
+            </div>
+          </div>
+        ))}
+      </div>
+      <button
+        className="flex flex-row justify-center items-center w-full group cursor-pointer z-20"
+        onClick={moveDown}
+      >
+        <ArrowDown className={arrowIconClasses} />
+      </button>
+    </div>
+  )
+}
